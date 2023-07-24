a5b86d03bf511335d02179e62a1020a0651c1d2b
bellobambo <bellobambo21@gmail.com>
2023-07-24 15:26:56 +0100

    sequential and functional API

--- neuralnextwork.py
+++ neuralnextwork.py
@@ -11,5 +11,63 @@ from tensorflow.keras.datasets import mnist
 # tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-print(x_train.shape)
-print(y_train.shape)
+x_train =  x_train.reshape(-1, 28*28).astype("float32") / 255.0
+x_test=  x_test.reshape(-1, 28*28).astype("float32") / 255.0
+
+#  Sequential API (very confinent but not very confortable)
+model = keras.Sequential(
+    [
+        keras.Input(shape=(28*28)),
+        layers.Dense(512, activation='relu'),
+        layers.Dense(256, activation='relu'),
+        layers.Dense(10),
+    ]
+)
+
+
+
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(784)))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(10))
+
+
+model = keras.Model(inputs=model.inputs, outputs=[layer.output for layer in model.layers ])
+
+features = model.predict(x_train)
+
+for feature in features:
+    print(feature.shape)
+
+
+import sys
+sys.exit()
+
+
+# functional API(flexible)
+
+inputs = keras.Input(shape=(784))
+x = layers.Dense(512, activation='relu', name='first_layer')(inputs)
+x = layers.Dense(256, activation='relu', name='second_layer')(x)
+outputs = layers.Dense(10, activation='softmax' )(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+print(model.summary())
+
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    optimizer=keras.optimizers.Adam(lr=0.001),
+    metrics=["accuracy"],
+)
+
+
+model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
+model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+
+
+
+# print(x_train.shape)
+# print(y_train.shape)
