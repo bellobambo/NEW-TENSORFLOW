0b05dbfad5a86356577a7dde42f27059eec98278
bellobambo <bellobambo21@gmail.com>
2023-07-26 18:24:24 +0100

    regularization

--- neuralnetwork2(convo).py
+++ neuralnetwork2(convo).py
@@ -3,11 +3,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 from tensorflow import keras
-import keras
-from keras import layers
-from tensorflow.keras.datasets import cifar10
-
-
+from keras import layers, regularizers
+from keras.datasets import cifar10
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.astype("float32") / 255.0
@@ -20,9 +17,9 @@ model = keras.Sequential(
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Conv2D(64, 3, activation='relu'),
         layers.MaxPooling2D(),
-        layers.Conv2D(128, 3, activation='relu'),  # Comma was missing here
+        layers.Conv2D(128, 3, activation='relu'),
         layers.Flatten(),
-        layers.Dense(64, activation='relu'),  # Comma was missing here
+        layers.Dense(64, activation='relu'),
         layers.Dense(10),
     ]
 )
@@ -30,30 +27,35 @@ model = keras.Sequential(
 
 def my_model():
     inputs = keras.Input(shape=(32, 32, 3))
-    x = layers.Conv2D(32, 3)(inputs)
+    x = layers.Conv2D(32,
+                       3, padding='same', kernel_regularizer=regularizers.l2(0.01),
+                       )(inputs)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    x = layers.MaxPooling2D()(x)  # Add () to the MaxPooling2D layer to call it properly
-    x = layers.Conv2D(64, 5, padding='same')(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(
+        64, 3, padding='same', kernel_regularizer=regularizers.l2(0.01))(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    x = layers.Conv2D(128, 3)(x)
+    x = layers.Conv2D(
+        128, 3,  padding='same', kernel_regularizer=regularizers.l2(0.01))(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    x = keras.Flatten()(x)
-    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Flatten()(x)  # Fixed the import for Flatten
+    x = layers.Dense(
+        64, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x)
+    x = layers.Dropout(0.5)(x)
     outputs = layers.Dense(10)(x)
-    model = keras.Model(inputs, outputs=outputs)
+    model = keras.Model(inputs=inputs, outputs=outputs)
     return model
-
 
 model = my_model()
 
 model.compile(
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer=keras.optimizers.Adam(lr=3e-4),
+    optimizer=keras.optimizers.Adam(learning_rate=3e-4),
     metrics=["accuracy"],
 )
 
-model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
-model.evaluate(x_test, y_test, batch_size=64, verbose=2)  # Fixed 'y_train' to 'y_test'
+model.fit(x_train, y_train, batch_size=64, epochs=150, verbose=2)
+model.evaluate(x_test, y_test, batch_size=64, verbose=2)
