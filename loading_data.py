9238bafee404c5157aaa7fd5ea524a61fbbe2b96
bellobambo <bellobambo21@gmail.com>
2023-07-31 23:43:24 +0100

    loading

---
+++ loading_data.py
@@ -0,0 +1,35 @@
+import tensorflow_datasets as tfds
+from tensorflow.keras import layers
+from tensorflow import keras
+import tensorflow as tf
+import os
+import matplotlib.pyplot
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+(ds_train, ds_test), ds_info = tfds.load(
+    "mnist",
+    split=["train", "test"],
+    shuffle_files=True,
+    as_supervised=False,
+    with_info=True
+)
+
+# fig = tfds.show_examples(ds_train, ds_info, rows=4, cols=4)
+# print(ds_info)
+
+def normalize_img(image, label):
+    return tf.cast(image, tf.float32)/225.0, label
+
+AUTOTUNE= tf.data.experimental.AUTOTUNE
+BATCH_SIZE = 64
+ds_train = ds_train.map(normalize_img, num_parallel_calls=AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits["train"].num_examples)
+ds_train = ds_train.batch(BATCH_SIZE)
+ds_train = ds_train.prefetch(AUTOTUNE)
+
+ds_test = ds_test.map(normalize_img, num_parallel_calls=AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.prefetch(AUTOTUNE)
+
