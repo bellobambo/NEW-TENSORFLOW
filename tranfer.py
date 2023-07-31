111f435df2b782a89e134e72ef038a68b8f69eb5
bellobambo <bellobambo21@gmail.com>
2023-07-31 04:20:19 +0100

    transfer

--- tranfer.py
+++ tranfer.py
@@ -25,6 +25,7 @@ model.trainable = False
 
 for layer in model.layers:
     assert layer.trainable == False
+    layers.trainable = False
 
 base_inputs = model.layers[0].input
 base_outputs = model.layers[-2].output
@@ -40,3 +41,51 @@ new_model.compile(
 )
 
 new_model.fit(x_train, y_train, batch_size=32, epochs=3, verbose=2)
+
+
+# Pretrained Keras Model
+
+x = tf.random.normal(shape-(5,299,299,3))
+x = tf.constant([0,1,2,3,4])
+
+
+model = keras.application.InceptionV3(include_top=True)
+
+base_input = model.layers[0].input
+base_outputs = model.layers[-2].output
+final_outputs = layers.Dense(5)(base_outputs)
+new_model = keras.Model(inputs=base_inputs, base_outputs=final_outputs)
+
+
+new_model = keras.Model(inputs=base_inputs, outputs=classifier)
+new_model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+# new_model.fit(x, y, epochs=15, verbose=2)
+
+# pretrained hub model
+
+x = tf.random.normal(shape=(5, 299, 299, 3))
+y = tf.constant([0,1,2,3,4])
+
+url = 'https://tfhub.dev/sayakpaul/swin_base_patch4_window12_384/1'
+
+base_model = hub.KerasLayer(url, input_shape=(299,299, 3))
+base_model.trainable = False
+model = keras.Sequential([
+    base_model,
+    layers.Dense(128, activation='relu'),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(5)
+])
+
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model.fit(x, y, batch_size=32, epochs=15, verbose=2)
