f57908a0dfb8c50a62d607c26b7eb1e3dd995599
bellobambo <bellobambo21@gmail.com>
2023-08-01 10:25:29 +0100

    data_augmentation

--- loading_data.py
+++ loading_data.py
@@ -11,7 +11,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
     "mnist",
     split=["train", "test"],
     shuffle_files=True,
-    as_supervised=False,
+    as_supervised=True,
     with_info=True
 )
 
@@ -33,3 +33,18 @@ ds_test = ds_test.map(normalize_img, num_parallel_calls=AUTOTUNE)
 ds_test = ds_test.batch(128)
 ds_test = ds_test.prefetch(AUTOTUNE)
 
+model = keras.Sequential([
+    keras.Input((28,28, 1)),
+    layers.Conv2D(32, 3,activation='relu'),
+    layers.Flatten(),
+    layers.Dense(10),
+])
+
+model.compile(
+    optimizer=keras.optimizers.Adam(lr=0.001),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"]
+)
+
+model.fit(ds_train, epochs=5)
+model.evaluate(ds_test)
